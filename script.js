26e74810e27cc962f6c66112606d8e72b180571f
Brian Robinson <brob92993@gmail.com>
2021-05-11 12:37:49 -0400

    Reorganized some of the javascript code for readability

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 // Variables for the user to input
 
-var enter;
+var generateBtn = document.querySelector("#generate");
+var enter = 0;
 var confirmNumber = true;
 var confirmCharacter = true;
 var confirmUppercase = true;
@@ -18,9 +19,7 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-
 var choices = [];
-var generateBtn = document.querySelector("#generate");
 
 
 // Add event listener to generate button
@@ -33,7 +32,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
@@ -41,52 +39,69 @@ function generatePassword() {
   enter = parseInt(prompt("How many Characters would you like your password to contain?"));
 if (!enter) {
   alert("This needs a value");
+
 } else if (enter < 8 || enter > 128) {
   enter = parseInt(prompt( "you must choose between 8 and 128"));
+
 } else {
   confirmNumber = window.confirm("Will this contain numbers?");
   confirmCharacter = window.confirm ("Will this contain special characters?");
   confirmLowercase = window.confirm ("will this contain lowercase letters?");
   confirmUppercase = window.confirm ("Will this contain uppercase letters?");
- } 
- if (!confirmNumber && !confirmCharacter && !confirmLowercase && !confirmUppercase) {
+ }
+ //conditional statments for each choice of what your password will contain 
+ 
+if (!confirmNumber && !confirmCharacter && !confirmLowercase && !confirmUppercase) {
   alert("Please choose criteria");
 }
 else if (confirmCharacter && confirmNumber && confirmLowercase && confirmUppercase) {
 choices = character.concat(number, lower,upper);
 console.log(choices);
+
 } 
 else if (confirmCharacter && confirmNumber && confirmUppercase) {
 choices = character.concat(number, upper);  
+
 } 
 else if (confirmCharacter && confirmNumber && confirmLowercase) {
 choices = character.concat(number, lower);  
+
 }
 else if (confirmNumber && confirmUppercase && confirmLowercase) {
 choices = number.concat(upper, lower);  
+
 }
 else if (confirmCharacter && confirmUppercase) {
 choices = character.concat (upper);  
+
 }
 else if (confirmCharacter && confirmLowercase) {
 choices = character.concat (lower);  
+
 }
 else if (confirmCharacter && confirmNumber) {
 choices = character.concat(number);  
+
 }
 else if (confirmCharacter) {
 choices = character;  
+
 }
 else if (confirmUppercase) {
 choices = upper;  
+
 }
 else if (confirmLowercase) {
 choices = lower;  
+
 }
 else if (confirmNumber) {
 choices = number;  
-}
-}
+
+} return(choices);
+} 
+
+
 
 
 
